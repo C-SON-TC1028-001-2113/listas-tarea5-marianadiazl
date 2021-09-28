2fe8305a769e964a94a555b7635e9167176afcb5
marianadiazl <a01252731@itesm.mx>
2021-09-28 06:26:53 +0000

    c

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,8 +1,22 @@
-
+def menoresDiez(x) :
+    m = []
+    for i in range(len(x) ):
+        for j in range(len(x[i])) :
+            if x[i][j]<10 :
+                m.append(x[i][j])
+    return(m)
 
 def main():
-
-
+    lista = []
+    fila = int(input())
+    columna = int(input())
+    for i in range(fila) :
+        lista.append([])
+        for j in range(columna) :
+            n = int(input())
+            lista[i].append(n)
+    r = menoresDiez(lista)
+    print(r)
 
 
 if __name__=='__main__':
