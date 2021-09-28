69b96cdc5b24f146d523191bb76301a24068e42f
marianadiazl <a01252731@itesm.mx>
2021-09-28 06:33:46 +0000

    m

--- assignments/08SinDuplicados/src/exercise.py
+++ assignments/08SinDuplicados/src/exercise.py
@@ -1,8 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    a = int(input())
+    lista = []
+    i = 0
+    if a>0 :
+        while i<a :
+            valor = input()
+            lista.append(valor)
+            i = i+1
+        print(lista)
+        noduplicados=[]
+        for i in lista :
+            if i not in noduplicados:
+                noduplicados.append(i)
+            print(noduplicados)
+    else:
+        print('Error')
 
     pass
 
 if __name__=='__main__':
     main()
+    
